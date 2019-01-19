a79487ef42c55384d1f4cb4f91d499a1ff2c5daf
smhx <stevenmai62@gmail.com>
2019-01-19 12:20:09 -0500

    button template

--- functions/commands/create_bet.js
+++ functions/commands/create_bet.js
@@ -17,10 +17,46 @@ const lib = require('lib')({ token: process.env.STDLIB_TOKEN });
 */
 module.exports = (user, channel, text = '', command = {}, botToken = null, callback) => {
     callback(null, {
-        text: `Hello, <@${user}>...\nYou said: ${text}`,
+        text: `Create bet, <@${user}>...\nYou said: ${text}`,
+        // attachments: [
+        // You can customize your messages with attachments.
+        // See https://api.slack.com/docs/message-attachments for more info.
+        // ]
         attachments: [
-            // You can customize your messages with attachments.
-            // See https://api.slack.com/docs/message-attachments for more info.
+            {
+                "text": "Choose a game to play",
+                "fallback": "You are unable to choose a game",
+                "callback_id": "wopr_game",
+                "color": "#3AA3E3",
+                "attachment_type": "default",
+                "actions": [
+                    {
+                        "name": "game",
+                        "text": "Chess",
+                        "type": "button",
+                        "value": "chess"
+                    },
+                    {
+                        "name": "game",
+                        "text": "Falken's Maze",
+                        "type": "button",
+                        "value": "maze"
+                    },
+                    {
+                        "name": "game",
+                        "text": "Thermonuclear War",
+                        "style": "danger",
+                        "type": "button",
+                        "value": "war",
+                        "confirm": {
+                            "title": "Are you sure?",
+                            "text": "Wouldn't you prefer a good game of chess?",
+                            "ok_text": "Yes",
+                            "dismiss_text": "No"
+                        }
+                    }
+                ]
+            }
         ]
     });
 };
